913aeaff02352d6cf80a6948a33e3d6fc44c4238
Udayasri Madadi <udayasrimadadi075@gmail.com>
2026-09-02 17:50:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,21 @@
 from flask import Flask, render_template, jsonify
-import random
 
 app = Flask(__name__)
+
+patient = {
+    "heart_rate": 78,
+    "spo2": 98,
+    "temperature": 36.7,
+    "respiratory_rate": 16
+}
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/api/vitals")
 def vitals():
-    heart_rate = random.randint(65, 100)
-    spo2 = random.randint(95, 100)
-    temperature = round(random.uniform(36.2, 37.5), 1)
-
-    emergency = (
-        heart_rate < 50 or heart_rate > 120
-        or spo2 < 90
-        or temperature < 35 or temperature > 39
-    )
-
-    return jsonify({
-        "heart_rate": heart_rate,
-        "spo2": spo2,
-        "temperature": temperature,
-        "emergency": emergency
-    })
-
+    return jsonify(patient)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
